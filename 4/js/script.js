27a75f534e3e103779fe840b3eb137c68240522c
Keks <keks@htmlacademy.ru>
2023-04-21 16:51:32 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/script.js
@@ -0,0 +1,14 @@
+let navMain = document.querySelector('.nav');
+let navToggle = document.querySelector('.nav__toggle');
+
+navMain.classList.remove('nav--no-js');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('nav--closed')) {
+    navMain.classList.remove('nav--closed');
+    navMain.classList.add('nav--opened');
+  } else {
+    navMain.classList.add('nav--closed');
+    navMain.classList.remove('nav--opened');
+  }
+});
